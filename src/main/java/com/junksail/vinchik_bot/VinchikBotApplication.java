47ada6b873735e88f66eb443c1ff347141072d9c
Хардбасс <junksail511@gmail.com>
2024-07-26 12:57:51 +0300

    залил проект на гит

---
+++ src/main/java/com/junksail/vinchik_bot/VinchikBotApplication.java
@@ -0,0 +1,13 @@
+package com.junksail.vinchik_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinchikBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinchikBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junksail/vinchik_bot/VinchikBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junksail.vinchik_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinchikBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
